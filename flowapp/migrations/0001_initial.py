2013344e72113b1ad640b5235ee3907886fa4f90
Mark Smith <mark@qq.is>
2015-07-31 23:32:26 -0700

    Add more models
    This fleshes out some more of the models to represent things. This has
    deviated slightly from the README (which is hopeful) and is aimed at
    getting up a MVP with basic functionality.
    
    Obviously, there's no way this is 100% correct. It's a WAG.

--- flowapp/migrations/0001_initial.py
+++ flowapp/migrations/0001_initial.py
@@ -14,12 +14,52 @@ class Migration(migrations.Migration):
             name='Account',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('name', models.CharField(max_length=128)),
+                ('name', models.CharField(unique=True, max_length=128)),
                 ('email', models.EmailField(max_length=254)),
                 ('api_key', models.CharField(max_length=128)),
                 ('created', models.DateTimeField(auto_now_add=True)),
                 ('accounttype', models.IntegerField(default=1)),
                 ('accountstatus', models.IntegerField(default=0)),
             ],
+        ),
+        migrations.CreateModel(
+            name='Event',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('created', models.DateTimeField(auto_now_add=True)),
+                ('content', models.TextField()),
+                ('author', models.ForeignKey(to='flowapp.Account', null=True)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Tag',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(unique=True, max_length=128)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Topic',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(unique=True, max_length=128)),
+                ('parent', models.ForeignKey(blank=True, to='flowapp.Topic', null=True)),
+                ('tags', models.ManyToManyField(to='flowapp.Tag')),
+            ],
+        ),
+        migrations.AddField(
+            model_name='event',
+            name='tags',
+            field=models.ManyToManyField(to='flowapp.Tag'),
+        ),
+        migrations.AddField(
+            model_name='event',
+            name='topic',
+            field=models.ForeignKey(to='flowapp.Topic'),
+        ),
+        migrations.AddField(
+            model_name='account',
+            name='topic',
+            field=models.ForeignKey(to='flowapp.Topic'),
         ),
     ]
--- flowapp/models.py
+++ flowapp/models.py
@@ -2,34 +2,65 @@ from django.db import models
 from django_enumfield import enum
 
 class AccountType(enum.Enum):
-	ROLE = 0
-	USER = 1
+    ROLE = 0
+    USER = 1
 
 class AccountStatus(enum.Enum):
-	ENABLED = 0
-	DISABLED = 1
+    ENABLED = 0
+    DISABLED = 1
 
 class Account(models.Model):
-	"""Account is someone that might author content
+    """Account is someone that might author content
 
-	This might be a person or a role account or some other account type that we
-	don't have today.
-	"""
-	name = models.CharField(max_length=128)
-	email = models.EmailField()
-	api_key = models.CharField(max_length=128)
-	created = models.DateTimeField(auto_now_add=True)
-	accounttype = enum.EnumField(AccountType, default=AccountType.USER)
-	accountstatus = enum.EnumField(AccountStatus, default=AccountStatus.ENABLED)
+    This might be a person or a role account or some other account type that we
+    don't have today.
+    """
+    name = models.CharField(max_length=128, unique=True)
+    email = models.EmailField()
+    api_key = models.CharField(max_length=128)
+    created = models.DateTimeField(auto_now_add=True)
+    accounttype = enum.EnumField(AccountType, default=AccountType.USER)
+    accountstatus = enum.EnumField(AccountStatus, default=AccountStatus.ENABLED)
+    topic = models.ForeignKey('Topic')
 
-	def is_user(self):
-		return self.accounttype == AccountType.USER
+    def is_user(self):
+        return self.accounttype == AccountType.USER
 
-	def is_role_account(self):
-		return self.accounttype == AccountType.ROLE
+    def is_role_account(self):
+        return self.accounttype == AccountType.ROLE
 
-	def is_enabled(self):
-		return self.accountstatus == AccountStatus.ENABLED
+    def is_enabled(self):
+        return self.accountstatus == AccountStatus.ENABLED
 
-	def is_disabled(self):
-		return self.accountstatus == AccountStatus.DISABLED
+    def is_disabled(self):
+        return self.accountstatus == AccountStatus.DISABLED
+
+class Topic(models.Model):
+    """Topic is the unit of organization of content
+
+    Right now you can't delete or hide topics. This is by design.
+    """
+    name = models.CharField(max_length=128, unique=True)
+    parent = models.ForeignKey('self', null=True, blank=True)
+    tags = models.ManyToManyField('Tag')
+
+class Tag(models.Model):
+    """Tags are used in various places as metadata
+
+    """
+    name = models.CharField(max_length=128, unique=True)
+
+class Event(models.Model):
+    """Events are the core bit of information
+
+    Think of them as tweets. They carry a payload of text, some tags, and are
+    published within a Topic.
+
+    We presently don't support any type of event deletion or hiding. What's
+    published stays forever... for now.
+    """
+    author = models.ForeignKey('Account', null=True)
+    topic = models.ForeignKey('Topic')
+    created = models.DateTimeField(auto_now_add=True)
+    tags = models.ManyToManyField('Tag')
+    content = models.TextField()
